c660faa2a8fc384661eff03db9bcb77f8db79c5b
Ivan Indrakusuma <ivan_indrakusuma@yahoo.com>
2021-06-24 10:49:08 +0700

    Add download option, add IT resources, update data level

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import csv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-class InputUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    lvl_existing = db.Column(db.Integer, nullable=False)
-    lvl_tujuan = db.Column(db.Integer, nullable=False)
-    it_res = db.Column(db.String(100), nullable=False)
-    it_process = db.Column(db.String(100), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 def index():
@@ -47,7 +36,24 @@ def input():
                     else:
                         first_line=False
             
-            return render_template('hasil.html', gap_analysis=gap_analysis)
+            with open('it_resource.csv') as data_itres:
+                data = csv.reader(data_itres, delimiter=';')
+                first_line = True
+                hasil = []
+                for row in data:
+                    for gap in gap_analysis:
+                        if not first_line and row[1]==it_res and gap['it_pro']==row[2]:
+                            hasil.append({
+                                "it_pro": gap['it_pro'],
+                                "awal": gap['awal'],
+                                "tujuan": gap['tujuan'],
+                                "task": gap['task']
+                            })
+                        
+                        else:
+                            first_line = False
+
+            return render_template('hasil.html', gap_analysis=hasil)
         except:
             return redirect('/input')
     else:
